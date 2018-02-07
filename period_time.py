ebbc17c7db782f9c969a55caba39571f6e25fc23
collin <collin.inbox@gmail.com>
2018-02-07 00:16:35 -0500

    wrote all the code in my logic defined at top of period_time.py

--- period_time.py
+++ period_time.py
@@ -21,7 +21,7 @@
         - get all stoppage events
         - stoppage.count > 0
             - find the last stoppage
-            - is stoppage in the same period
+            - if stoppage in the same period
                 - if currentPeriodTimeRemaining == periodTimeRemaining +/- 1 second
                   i saw be periodTimeRemaining 2 while currentPeriodTimeRemaining was 1
                     - set time to periodTimeRemaining
@@ -33,34 +33,21 @@
 import json
 
 
+def get_seconds_from_string(time_str):
+    """convert minute:second string time to int seconds
+    print get_seconds_from_string('10:45')
+    print get_seconds_from_string('20:00')
+    print get_seconds_from_string('03:14')
+    https://stackoverflow.com/a/6402859/1469690
+    """
+    minute, second = time_str.split(':')
+    return int(minute) * 60 + int(second)
+
+
 def get_game_data_from_file(file_name):
     """imports json data
     """
     return json.load(open(file_name))
-
-
-def get_parsed_game_data(game_data):
-    """parses game data
-    """
-    game = {}
-
-    game['abstractGameState'] = (
-        game_data['gameData']['status']['abstractGameState']
-    )
-
-    game['currentPeriodOrdinal'] = (
-        game_data['liveData']['linescore']['currentPeriodOrdinal']
-    )
-
-    game['currentPeriodTimeRemaining'] = (
-        game_data['liveData']['linescore']['currentPeriodTimeRemaining']
-    )
-
-    game['intermissionTimeRemaining'] = (
-        game_data['liveData']['linescore']['intermissionInfo']['intermissionTimeRemaining']
-    )
-
-    return game
 
 
 def get_all_stoppage(game_data):
@@ -91,6 +78,38 @@ def get_all_stoppage(game_data):
     return stoppage
 
 
+def get_parsed_game_data(game_data):
+    """parses game data
+    """
+    game = {}
+
+    game['abstractGameState'] = (
+        game_data['gameData']['status']['abstractGameState']
+    )
+
+    game['currentPeriod'] = (
+        game_data['liveData']['linescore']['currentPeriod']
+    )
+
+    game['currentPeriodOrdinal'] = (
+        game_data['liveData']['linescore']['currentPeriodOrdinal']
+    )
+
+    game['currentPeriodTimeRemaining'] = (
+        game_data['liveData']['linescore']['currentPeriodTimeRemaining']
+    )
+
+    game['intermissionTimeRemaining'] = (
+        game_data['liveData']['linescore']['intermissionInfo']['intermissionTimeRemaining']
+    )
+
+    game['stoppage'] = (
+        get_all_stoppage(game_data)
+    )
+
+    return game
+
+
 def get_game_time_and_period(game_data):
     """figures out what the time and period should be
     uses parsed game data from get_parsed_game_data
@@ -99,35 +118,56 @@ def get_game_time_and_period(game_data):
 
     # Game has not started
     if game_data['abstractGameState'] == 'Preview':
-        info['time'] = '00:00'
         info['period'] = "PRE"
+        info['time'] = '00:00'
 
     # Game over
     elif game_data['abstractGameState'] == 'Final':
-        info['time'] = '00:00'
         info['period'] = "END"
+        info['time'] = '00:00'
 
     # Intermission
     elif (game_data['abstractGameState'] == 'Live' and
           game_data['intermissionTimeRemaining'] != 0):
-        info['time'] = '00:00'
         info['period'] = "InT"
+        info['time'] = '20:00'
 
     # Game is live
     elif (game_data['abstractGameState'] == 'Live' and
           game_data['intermissionTimeRemaining'] == 0):
-        info['time'] = game_data['currentPeriodTimeRemaining']
         info['period'] = game_data['currentPeriodOrdinal']
+        if len(game_data['stoppage']) >= 1:
+            # print 'has stoppage'
+            latest_stoppage = game_data['stoppage'][-1]
+            latest_stoppage_period = latest_stoppage['about']['period']
+            latest_stoppage_time_remaining = latest_stoppage['about']['periodTimeRemaining']
+            if game_data['currentPeriod'] == latest_stoppage_period:
+                time_remaining_seconds = get_seconds_from_string(
+                    game_data['currentPeriodTimeRemaining']
+                )
+                stoppage_time_remaining_seconds = get_seconds_from_string(
+                    latest_stoppage_time_remaining
+                )
+                if (time_remaining_seconds == stoppage_time_remaining_seconds or
+                        time_remaining_seconds == (stoppage_time_remaining_seconds + 1) or
+                        time_remaining_seconds == (stoppage_time_remaining_seconds - 1)):
+                    print 'game is stopped'
+                    info['time'] = game_data['currentPeriodTimeRemaining']
+                else:
+                    print 'start counter'
+        else:
+            #print 'no stoppage yet in game'
+            info['time'] = game_data['currentPeriodTimeRemaining']
+            print 'start counter'
 
     return info
 
 
 GAME_DATA = get_game_data_from_file('exampleDataGameLive.json')
+#GAME_DATA = get_game_data_from_file('exampleDataGameStopped.json')
 
 PARSED_GAME_DATA = get_parsed_game_data(GAME_DATA)
 
 GAME_TIME_AND_PERIOD = get_game_time_and_period(PARSED_GAME_DATA)
 
-# print GAME_TIME_AND_PERIOD
-
-print get_all_stoppage(GAME_DATA)
+print GAME_TIME_AND_PERIOD
