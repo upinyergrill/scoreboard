bc26613cd5eb35989b1d362e30b893fcb9ef9a8b
collin <collin.inbox@gmail.com>
2018-02-06 22:56:41 -0500

    basics of the time and period logic done. still have to do the stoppage

--- period_time.py
+++ period_time.py
@@ -28,3 +28,64 @@
         - else
             - start counter at currentPeriodTimeRemaining
 '''
+import json
+
+
+def get_game_data_from_file(file_name):
+    """imports json data
+    """
+    return json.load(open(file_name))
+
+
+def get_parsed_game_data(game_data):
+    """parses game data
+    """
+    game = {}
+
+    game['abstractGameState'] = game_data['gameData']['status']['abstractGameState']
+
+    game['currentPeriodOrdinal'] = (
+        game_data['liveData']['linescore']['currentPeriodOrdinal'])
+
+    game['currentPeriodTimeRemaining'] = (
+        game_data['liveData']['linescore']['currentPeriodTimeRemaining'])
+
+    game['intermissionTimeRemaining'] = (
+        game_data['liveData']['linescore']['intermissionInfo']['intermissionTimeRemaining']
+    )
+
+    return game
+
+
+def get_game_time_and_period(game_data):
+    """figures out what the time and period should be
+    """
+    info = {'time': '', 'period': ''}
+
+    if game_data['abstractGameState'] == 'Preview':
+        info['time'] = '00:00'
+        info['period'] = "PRE"
+
+    elif game_data['abstractGameState'] == 'Final':
+        info['time'] = '00:00'
+        info['period'] = "END"
+
+    elif (game_data['abstractGameState'] == 'Live' and
+          game_data['intermissionTimeRemaining'] != 0):
+        info['time'] = '00:00'
+        info['period'] = "InT"
+
+    elif (game_data['abstractGameState'] == 'Live' and
+          game_data['intermissionTimeRemaining'] == 0):
+        info['time'] = game_data['currentPeriodTimeRemaining']
+        info['period'] = game_data['currentPeriodOrdinal']
+
+    return info
+
+GAME_DATA = get_game_data_from_file('exampleDataGameLive.json')
+
+PARSED_GAME_DATA = get_parsed_game_data(GAME_DATA)
+
+GAME_TIME_AND_PERIOD = get_game_time_and_period(PARSED_GAME_DATA)
+
+print GAME_TIME_AND_PERIOD
