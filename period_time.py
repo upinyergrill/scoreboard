87c743de621f3eb15cba967533cdc2f8479293fb
collin <collin.inbox@gmail.com>
2018-02-06 23:41:13 -0500

    wrote code for gettign all stoppage

--- period_time.py
+++ period_time.py
@@ -12,8 +12,10 @@
         - set time to 00:00
          - set period to END
     - else if (abstractGameState == Live) && (intermissionTimeRemaining != 0) (intermission)
-        - set time to 00:00
+        - set time to intermissionTimeRemaining
         - set period to InT
+        - start counter at intermissionTimeRemaining
+          not sure what intermissionTimeRemaining looks like when not 0, might be in minutes
     - else if (abstractGameState == Live) && (intermissionTimeRemaining == 0) (game is live)
         - set period to currentPeriodOrdinal
         - get all stoppage events
@@ -42,13 +44,17 @@ def get_parsed_game_data(game_data):
     """
     game = {}
 
-    game['abstractGameState'] = game_data['gameData']['status']['abstractGameState']
+    game['abstractGameState'] = (
+        game_data['gameData']['status']['abstractGameState']
+    )
 
     game['currentPeriodOrdinal'] = (
-        game_data['liveData']['linescore']['currentPeriodOrdinal'])
+        game_data['liveData']['linescore']['currentPeriodOrdinal']
+    )
 
     game['currentPeriodTimeRemaining'] = (
-        game_data['liveData']['linescore']['currentPeriodTimeRemaining'])
+        game_data['liveData']['linescore']['currentPeriodTimeRemaining']
+    )
 
     game['intermissionTimeRemaining'] = (
         game_data['liveData']['linescore']['intermissionInfo']['intermissionTimeRemaining']
@@ -57,24 +63,57 @@ def get_parsed_game_data(game_data):
     return game
 
 
+def get_all_stoppage(game_data):
+    """gets all stoppage events from unparsed game data
+    """
+    all_plays = game_data['liveData']['plays']['allPlays']
+
+    # Will return [] if none
+    stoppage = [d for d in all_plays if d['result']['event'] == 'Stoppage']
+
+    '''
+    prices = {
+        'ACME': 45.23,
+        'AAPL': 612.78,
+        'IBM': 205.55,
+        'HPQ': 37.20,
+        'FB': 10.75
+    }
+
+    # Make a dictionary of all prices over 200
+    one = {key: value for key, value in prices.items() if value > 200}
+
+    # Make a dictionary of tech stocks
+    tech_names = {'AAPL', 'IBM', 'HPQ', 'MSFT'}
+    two = {key: value for key, value in prices.items() if key in tech_names}
+    '''
+
+    return stoppage
+
+
 def get_game_time_and_period(game_data):
     """figures out what the time and period should be
+    uses parsed game data from get_parsed_game_data
     """
     info = {'time': '', 'period': ''}
 
+    # Game has not started
     if game_data['abstractGameState'] == 'Preview':
         info['time'] = '00:00'
         info['period'] = "PRE"
 
+    # Game over
     elif game_data['abstractGameState'] == 'Final':
         info['time'] = '00:00'
         info['period'] = "END"
 
+    # Intermission
     elif (game_data['abstractGameState'] == 'Live' and
           game_data['intermissionTimeRemaining'] != 0):
         info['time'] = '00:00'
         info['period'] = "InT"
 
+    # Game is live
     elif (game_data['abstractGameState'] == 'Live' and
           game_data['intermissionTimeRemaining'] == 0):
         info['time'] = game_data['currentPeriodTimeRemaining']
@@ -82,10 +121,13 @@ def get_game_time_and_period(game_data):
 
     return info
 
+
 GAME_DATA = get_game_data_from_file('exampleDataGameLive.json')
 
 PARSED_GAME_DATA = get_parsed_game_data(GAME_DATA)
 
 GAME_TIME_AND_PERIOD = get_game_time_and_period(PARSED_GAME_DATA)
 
-print GAME_TIME_AND_PERIOD
+# print GAME_TIME_AND_PERIOD
+
+print get_all_stoppage(GAME_DATA)
