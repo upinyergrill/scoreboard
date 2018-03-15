aee0caa6996c872173ab8c13069fafa8103b82c8
collin <collin.inbox@gmail.com>
2018-03-14 23:02:59 -0400

    scoll not a class anymore

--- scrollable_text.py
+++ scrollable_text.py
@@ -1,23 +1,21 @@
 import time
 from rgbmatrix import graphics, RGBMatrix, RGBMatrixOptions
 
-
-class ScrollableText(object):
-    def scroll(self, matrix, font, y, color, message, border_pixels=None, border_color=None):
-        if not border_pixels:
-            border_pixels = []
-        pos = 65
-        char_height = 5
-        message_len = list(range(len(message) * 4 + 65))
-        for _ in message_len:
-            for x_idx, _ in enumerate(list(range(64))):
-                for y_idx, _ in enumerate(list(range(char_height))):
-                    y_pixel = y - 1 - y_idx
-                    if [x_idx, y_pixel] not in border_pixels:
-                        matrix.SetPixel(x_idx, y_pixel, 0, 0, 0)
-            graphics.DrawText(matrix, font, pos, y, color, message)
-            if border_pixels and border_color:
-                for pixel in border_pixels:
-                    matrix.SetPixel(pixel[0], pixel[1], border_color[0], border_color[1], border_color[2])
-            pos = pos - 1
-            time.sleep(0.05)
+def scroll(matrix, font, y, color, message, border_pixels=None, border_color=None):
+    if not border_pixels:
+        border_pixels = []
+    pos = 65
+    char_height = 5
+    message_len = list(range(len(message) * 4 + 65))
+    for _ in message_len:
+        for x_idx, _ in enumerate(list(range(64))):
+            for y_idx, _ in enumerate(list(range(char_height))):
+                y_pixel = y - 1 - y_idx
+                if [x_idx, y_pixel] not in border_pixels:
+                    matrix.SetPixel(x_idx, y_pixel, 0, 0, 0)
+        graphics.DrawText(matrix, font, pos, y, color, message)
+        if border_pixels and border_color:
+            for pixel in border_pixels:
+                matrix.SetPixel(pixel[0], pixel[1], border_color[0], border_color[1], border_color[2])
+        pos = pos - 1
+        time.sleep(0.05)
