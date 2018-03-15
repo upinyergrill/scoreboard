91add8961140eb78cf5f6b9acdfedb90ea97c224
collin <collin.inbox@gmail.com>
2018-03-14 23:01:29 -0400

    testing scroll

--- scrollable_text.py
+++ scrollable_text.py
@@ -3,41 +3,6 @@ from rgbmatrix import graphics, RGBMatrix, RGBMatrixOptions
 
 
 class ScrollableText(object):
-    def __init__(self, options=None):
-        if options:
-            if not isinstance(options, RGBMatrixOptions):
-                raise ValueError('options must be type RGBMatrixOptions')
-        else:
-            # RGBMatrix Options
-            options = RGBMatrixOptions()
-            options.rows = 32
-            options.chain_length = 2
-            options.brightness = 30
-            options.gpio_slowdown = 2
-            options.drop_privileges = 0
-
-        self.matrix = RGBMatrix(options=options)
-
-    def run(self, message):
-        offscreen_canvas = self.matrix.CreateFrameCanvas()
-        font = graphics.Font()
-        font.LoadFont("Assets/tom-thumb.bdf")
-        text_color = graphics.Color(255, 255, 255)
-        pos = offscreen_canvas.width
-        my_text = message
-        counter = (len(my_text) * 4 + 65) * 3
-        while counter >= 0:
-            offscreen_canvas.Clear()
-            lens = graphics.DrawText(
-                offscreen_canvas, font, pos, 31, text_color, my_text)
-            pos -= 1
-            if pos + lens < 0:
-                pos = offscreen_canvas.width
-
-            time.sleep(0.05)
-            offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
-            counter -= 1
-
     def scroll(self, matrix, font, y, color, message, border_pixels=None, border_color=None):
         if not border_pixels:
             border_pixels = []
