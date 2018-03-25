d32ffcc8aa158accaec822bf820b8ded1149a194
collin <collin.inbox@gmail.com>
2018-03-25 19:54:54 -0400

    test faceoffs

--- nhl_board_render.py
+++ nhl_board_render.py
@@ -103,7 +103,7 @@ def draw_live_helper(matrix, font, color, game_data):
     draw_home_team_score(matrix, font, color, game_data)
     draw_away_team_score(matrix, font, color, game_data)
     draw_period(matrix, font, color, game_data)
-    draw_carousel_blocked(matrix, font, color, game_data)
+    draw_carousel_faceoff(matrix, font, color, game_data)
 
 def draw_carousel_sog(matrix, font, color, game_data):
     clear_area(matrix, 15, 30, 8, 6)
@@ -130,12 +130,11 @@ def draw_carousel_blocked(matrix, font, color, game_data):
     graphics.DrawText(matrix, font, 49, 30, color, str(game_data['homeBlocked']))
 
 def draw_carousel_faceoff(matrix, font, color, game_data):
-    away_FOWins = str(nhlgameinfo.awayFOWins())
-    home_FOWins = str(nhlgameinfo.homeFOWins())
-    clearInfo()
+    clear_area(matrix, 7, 30, 16, 6)
+    graphics.DrawText(matrix, font, 7, 30, color, str(game_data['awayFoWins']))
     graphics.DrawText(matrix, font, 26, 30, color, "FO%")
-    graphics.DrawText(matrix, font, 41, 30, color, home_FOWins)
-    graphics.DrawText(matrix, font, 7, 30, color, away_FOWins)
+    graphics.DrawText(matrix, font, 41, 30, color, str(game_data['homeFoWins']))
+    
 
 def draw_carousel_takeaway(matrix, font, color, game_data):
     away_takeaway = str(nhlgameinfo.awayTakeaways())
