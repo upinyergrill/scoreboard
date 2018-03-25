d333635bc177a8f8c8c62d56b1e57432ce616db0
collin <collin.inbox@gmail.com>
2018-03-24 22:30:41 -0400

    testing matrix

--- examples/test_multi_matrix.py
+++ examples/test_multi_matrix.py
@@ -1,6 +1,7 @@
 from rgbmatrix import RGBMatrix, graphics, RGBMatrixOptions
-from multiprocessing import Value, Process, Queue, Array
+import threading
 import time
+
 
 def subproc(matrix):
     matrix.SetPixel(25, 2, 0, 255, 0)
@@ -8,6 +9,40 @@ def subproc(matrix):
     matrix.SetPixel(25, 4, 0, 255, 0)
     matrix.SetPixel(25, 5, 0, 255, 0)
     print('print subproc')
+
+
+class myThread (threading.Thread):
+    def __init__(self, threadID, name, counter):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+
+    def run(self):
+        print("Starting " + self.name)
+        print_time(self.name, self.counter, 5)
+        print("Exiting " + self.name)
+
+
+def print_time(threadName, delay, counter):
+    while counter:
+        if exitFlag:
+            threadName.exit()
+        time.sleep(delay)
+        print("%s: %s" % (threadName, time.ctime(time.time())))
+        counter -= 1
+
+
+# Create new threads
+thread1 = myThread(1, "Thread-1", 1)
+thread2 = myThread(2, "Thread-2", 2)
+
+# Start new Threads
+thread1.start()
+thread2.start()
+thread1.join()
+thread2.join()
+print("Exiting Main Thread")
 
 # RGBMatrix Options
 options = RGBMatrixOptions()
@@ -26,7 +61,7 @@ matrix.SetPixel(23, 4, 0, 0, 255)
 matrix.SetPixel(23, 5, 0, 0, 255)
 print('print main')
 
-scroll_process = Process(target=subproc, args=(matrix,))
-scroll_process.start()
+#scroll_process = Process(target=subproc, args=(matrix,))
+#scroll_process.start()
 
-time.sleep(999)
+#time.sleep(999)
