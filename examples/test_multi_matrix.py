0ac5af3b7f156fa189781ca46b911e2d3fe65747
collin <collin.inbox@gmail.com>
2018-03-24 22:33:37 -0400

    testing matrix

--- examples/test_multi_matrix.py
+++ examples/test_multi_matrix.py
@@ -14,37 +14,18 @@ exitFlag = 0
 
 
 class myThread (threading.Thread):
-    def __init__(self, threadID, name, counter):
+    def __init__(self, threadID, name, matrix):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
-        self.counter = counter
+        self.matrix = matrix
 
     def run(self):
         print("Starting " + self.name)
-        print_time(self.name, self.counter, 5)
+        subproc(self.matrix)
         print("Exiting " + self.name)
 
 
-def print_time(threadName, delay, counter):
-    while counter:
-        if exitFlag:
-            threadName.exit()
-        time.sleep(delay)
-        print("%s: %s" % (threadName, time.ctime(time.time())))
-        counter -= 1
-
-
-# Create new threads
-thread1 = myThread(1, "Thread-1", 1)
-thread2 = myThread(2, "Thread-2", 2)
-
-# Start new Threads
-thread1.start()
-thread2.start()
-thread1.join()
-thread2.join()
-print("Exiting Main Thread")
 
 # RGBMatrix Options
 options = RGBMatrixOptions()
@@ -62,6 +43,15 @@ matrix.SetPixel(23, 3, 0, 0, 255)
 matrix.SetPixel(23, 4, 0, 0, 255)
 matrix.SetPixel(23, 5, 0, 0, 255)
 print('print main')
+
+# Create new threads
+thread1 = myThread(1, "Thread-1", matrix)
+
+# Start new Threads
+thread1.start()
+thread1.join()
+print("Exiting Main Thread")
+
 
 #scroll_process = Process(target=subproc, args=(matrix,))
 #scroll_process.start()
